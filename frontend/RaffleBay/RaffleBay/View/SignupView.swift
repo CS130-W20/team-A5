cfe6adc8146cbecd56dad856fd615538cfe9e266
Meera Rachamallu <mrachamallu@gmail.com>
2020-02-06 13:05:25 -0800

    added swiftyjson

---
+++ frontend/RaffleBay/RaffleBay/View/SignupView.swift
@@ -0,0 +1,25 @@
+//
+//  SignupView.swift
+//  RaffleBay
+//
+//  Created by Meera Rachamallu on 2/6/20.
+//  Copyright © 2020 Meera Rachamallu. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+import SwiftyJSON
+struct SignupView : View {
+
+    var body: some View {
+        VStack() {
+            Text("SignUp")
+        }
+    }
+}
+
+struct SignupView_Previews: PreviewProvider {
+    static var previews: some View {
+        SignupView()
+    }
+}
