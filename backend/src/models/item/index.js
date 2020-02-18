f53008b750baf295d3a78fdfe6e6062e8a2ef3dc
alexlongerbeam <alexlongerbeam@yahoo.com>
2020-02-18 13:52:47 -0800

    Implemented Create bid endpoint and all that comes with it (#56)
    * Implemented Create bid endpoint and all that comes with it
    
    * Readme updates and bug fixes
    
    * Added check so seller can't buy tickets

--- backend/src/models/item/index.js
+++ backend/src/models/item/index.js
@@ -1,8 +1,8 @@
 const ItemModel = (repo) => {
   // Creates an item with given info
-  const createItem= async (item_name, seller_id, pic_url, item_description, tags, sale_price, ticket_price, total_tickets, deadline, status, current_ledger) => {
+  const createItem= async (item_name, seller_id, pic_url, item_description, tags, sale_price, ticket_price, total_tickets, deadline, status) => {
 
-    const [item, err] = await repo.createItem(item_name, seller_id, pic_url, item_description, tags, sale_price, ticket_price, total_tickets, deadline, status, current_ledger);
+    const [item, err] = await repo.createItem(item_name, seller_id, pic_url, item_description, tags, sale_price, ticket_price, total_tickets, deadline, status);
 
     return [item, err];
   };
@@ -15,9 +15,16 @@ const ItemModel = (repo) => {
     return [item, err];
   };
 
+  // Calls repo method to create a bid. The repo method is atomic, will ensure bid can
+  // go through before creating
+  const createBid = async (user_id, item_id, ticket_count, total_cost) => {
+    return await repo.createBid(user_id, item_id, ticket_count, total_cost);
+  }
+
   return {
     createItem,
-    getItemInfo
+    getItemInfo,
+    createBid,
   };
 };
 
