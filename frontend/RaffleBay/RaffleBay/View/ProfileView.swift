4e8457202d00d2fd6b52131e220993c87e818e52
pmarks98 <piersonmarks@engineering.ucla.edu>
2020-02-17 21:39:37 -0800

    Begin work on Profile Page

---
+++ frontend/RaffleBay/RaffleBay/View/ProfileView.swift
@@ -0,0 +1,58 @@
+//
+//  ProfileView.swift
+//  RaffleBay
+//
+//  Created by Pierson Marks on 2/17/20.
+//  Copyright © 2020 Meera Rachamallu. All rights reserved.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        VStack(){
+            HStack(){
+                Spacer()
+                VStack(){
+                    Image("bose")
+                        .clipShape(Circle())
+                        .overlay(Circle().stroke(Color.white, lineWidth: 4))
+                        .shadow(radius: 7)
+                    Text("Pierson Marks")
+                        .clearButtonText()
+                    Text("Account Balance: $" + "47.00")
+                        .standardBoldText()
+                }
+                Spacer()
+            }
+            
+            HStack(){
+                Button(action: {
+                   
+                }){
+                   Text("Items Bid On")
+                       .standardBoldText()
+                       .padding(8)
+                }
+                
+                Button(action: {
+                    
+                }){
+                    Text("Items Listed")
+                        .standardBoldText()
+                        .padding(8)
+                }
+            }
+            
+            Spacer()
+        
+        }
+        
+    }
+}
+
+struct ProfileView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileView()
+    }
+}
