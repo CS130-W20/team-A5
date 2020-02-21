aaaeaed5b7ecaecb558d01651e9acbf456a5d0e0
Siddharth Satuluru <satulurusid98@gmail.com>
2020-02-21 05:35:23 -0800

    API testing code

---
+++ backend/src/test/admin.test.js
@@ -0,0 +1,10 @@
+const request = require('supertest');
+const app = require('../main'); 
+describe('Choose Winner', () => {
+	it('should only choose winners for items awaiting raffle', async () => {
+	
+	})
+	it('should only select one winner', async() => {
+	}) 
+
+})
