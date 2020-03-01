8e7b3476c535724c4024c26f694e36df32e4a918
Meera Rachamallu <mrachamallu@gmail.com>
2020-02-29 22:15:20 -0800

    removed comments

--- frontend/RaffleBay/RaffleBay/Controller/ProfileViewController.swift
+++ frontend/RaffleBay/RaffleBay/Controller/ProfileViewController.swift
@@ -65,34 +65,3 @@ func get_items_selling_and_bidding(auth_token: String, completion: @escaping (([
            }
        };
 }
-//func get_items_selling_and_bidding(auth_token: String, completion: @escaping (String) -> Void) -> (selling: [SellingItem], buying: [BuyingItem]) {
-//    var selling: [SellingItem] = []
-//    var buying: [BuyingItem] = []
-//    let headers: HTTPHeaders = [
-//           "Authorization": "Bearer \(auth_token)"
-//       ]
-//       Alamofire.request(url + "/api/items/me", method: .get, encoding: JSONEncoding.default, headers: headers)
-//       .responseSwiftyJSON { dataResponse in
-//           print(dataResponse.request)
-//           if dataResponse.result.isSuccess {
-//               let message = dataResponse.value!["message"]
-//               print("message: \(message)")
-//               if message == "" {
-//                   let data = dataResponse.value!["data"]
-//                   let auth = data["auth_token"]
-//                   print("data: \(data)")
-//                   print("auth: \(auth)")
-//                if let items_selling = data["items_selling"].array { print(items_selling)
-//                    for item in items_selling {
-//                        let sellingItem = SellingItem(item_name: item["item_name"].string!, pic_url: item["pic_url"].string!, sale_price: String(item["sale_price"].int!), created_at: item["created_at"].string!, tickets_sold: String(item["tickets_sold"].int!) )
-//                        selling.append(sellingItem)
-//                        print(sellingItem.item_name)
-//                    }
-//                }
-//               }
-//           } else {
-//               print(dataResponse.error)
-//           }
-//       };
-//    return (selling, buying)
-//}
