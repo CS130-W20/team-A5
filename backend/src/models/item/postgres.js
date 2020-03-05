b469c76af65c0f4e87c9170584ad68ce01b6f83a
alexlongerbeam <longerbeamalex@gmail.com>
2020-03-05 14:01:12 -0800

    Created Item feed endpoint (#81)
    * Made item feed
    
    * Proptypes Module update
    
    * Readme and fixes

--- backend/src/models/item/postgres.js
+++ backend/src/models/item/postgres.js
@@ -437,6 +437,33 @@ const ItemRepo = (postgres) => {
     }
   };
 
+  /**
+   * SQL to get a feed of items where the raffle is in progress
+   * @type {string}
+   */
+  const getItemFeedSQL = `
+   SELECT * FROM items
+   WHERE status=$1
+   ORDER BY deadline;
+  `;
+
+  /**
+   * Gets a feed of all in progress objects
+   * 
+   * @return {Array<{0: Array<Item>, 1: String}>} - Array with array of Rafflebay Item Objects and error (only one or the other)
+   */
+  const getItemFeed = async () => {
+    values=["IP"]
+    try {
+      const client = await postgres.connect();
+      const res = await client.query(getItemFeedSQL, values);
+      client.release();
+      return [res.rows, null];
+    } catch (err) {
+      return [null, err];
+    }
+  }
+
 
   return {
     setupRepo,
@@ -450,6 +477,7 @@ const ItemRepo = (postgres) => {
     getItemsForSeller,
     getBidsForUser,
     createShipment,
+    getItemFeed,
   };
 };
 
