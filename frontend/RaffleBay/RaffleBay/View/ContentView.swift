b49349496a5d3487b71962e2079442a2dc729689
Pierson Marks <piersonmarks@engineering.ucla.edu>
2020-03-06 20:23:35 -0800

    New Navigation Links (#86)
    * Implemented Basic Navigiation Stack to remove dependence on NavigationViews.
    
    * Adding in buttons
    
    * Add Navigation to Signup and Login
    
    * Check if Auth Token exists
    
    * Continued connecting Views
    
    * Hooked up remaining links.
    
    * Fixed merge conflicts

--- frontend/RaffleBay/RaffleBay/View/ContentView.swift
+++ frontend/RaffleBay/RaffleBay/View/ContentView.swift
@@ -8,33 +8,112 @@
 
 import SwiftUI
 
+//Creation of our own Navigation View
+struct NavigationItem{
+   var view: AnyView
+}
+
+final class NavigationStack: ObservableObject {
+    @Published var viewStack: [NavigationItem] = []
+    @Published var currentView: NavigationItem
+    
+    init(_ currentView: NavigationItem ){
+       self.currentView = currentView
+    }
+    
+    func unwind(){
+       if viewStack.count == 0{
+          return }
+       let last = viewStack.count - 1
+       currentView = viewStack[last]
+       viewStack.remove(at: last)
+    }
+    
+    func advance(_ view:NavigationItem){
+       viewStack.append( currentView)
+       currentView = view
+    }
+    
+    func home( ){
+       currentView = NavigationItem( view: AnyView(SaleItemTableView()))
+       viewStack.removeAll()
+    }
+    
+    func splashscreen( ){
+       currentView = NavigationItem( view: AnyView(SplashscreenView()))
+       viewStack.removeAll()
+    }
+    
+    func login( ){
+       currentView = NavigationItem( view: AnyView(LoginView()))
+       viewStack.removeAll()
+    }
+    
+    func signup( ){
+       currentView = NavigationItem( view: AnyView(SignupView()))
+       viewStack.removeAll()
+    }
+    
+    func success( ){
+       currentView = NavigationItem( view: AnyView(SuccessfulView()))
+       viewStack.removeAll()
+    }
+    
+    
+}
+
+struct NavigationHost: View{
+   @EnvironmentObject var navigation: NavigationStack
+   
+   var body: some View {
+      self.navigation.currentView.view
+   }
+}
+
+//Check if we have some auth token
+func whatView(token: String) -> AnyView{
+    if (token == ""){
+       return AnyView(SplashscreenView())
+    }else{
+        return AnyView(SaleItemTableView())
+  }
+    
+}
+
 struct ContentView: View {
-    @State private var selection = 0
+    //@State private var selection = 0
     @ObservedObject var authenticationVM = AuthenticationViewModel()
 
     var body: some View {
-        if self.authenticationVM.auth_token == "" {
-            return AnyView(SplashscreenView())
-        } else {
-            return AnyView(TabView(selection: $selection){
-                SaleItemTableView()
-                    .tabItem {
-                        VStack {
-                            Image("first")
-                            Text("First")
-                        }
-                    }
-                    .tag(0)
-                ProfileView()
-                    .tabItem {
-                        VStack {
-                            Image("second")
-                            Text("Second")
-                        }
-                    }
-                    .tag(1)
-            })
-        }
+        NavigationHost()
+        .environmentObject(NavigationStack(
+            NavigationItem( view: whatView(token: self.authenticationVM.auth_token))
+                            ))
+    
+        
+        
+//        if self.authenticationVM.auth_token == "" {
+//            return AnyView(SplashscreenView())
+//        } else {
+//            return AnyView(TabView(selection: $selection){
+//                SaleItemTableView()
+//                    .tabItem {
+//                        VStack {
+//                            Image("first")
+//                            Text("First")
+//                        }
+//                    }
+//                    .tag(0)
+//                ProfileView()
+//                    .tabItem {
+//                        VStack {
+//                            Image("second")
+//                            Text("Second")
+//                        }
+//                    }
+//                    .tag(1)
+//            })
+//        }
 
     }
 }
--- frontend/RaffleBay/RaffleBay/View/Purchase Item Views/SaleItemDetailView.swift
+++ frontend/RaffleBay/RaffleBay/View/Purchase Item Views/SaleItemDetailView.swift
@@ -10,77 +10,92 @@ import Foundation
 import SwiftUI
 
 struct SaleItemDetailView : View {
+    @EnvironmentObject var navigation: NavigationStack
+
     @State private var num_of_tickets: Int? = 0
 //    let saleItem: SaleItem
     var body: some View {
         VStack(){
-            Spacer().frame(height: 60)
-            VStack(alignment: .leading) {
-                Image("bose")
-                    .resizable()
-                    .frame(maxWidth: 350, maxHeight: 200)
-                Text("Bose QuietComfort 100")
-                    .h1()
-                HStack(alignment: .top){
+            HStack(){
+                Button(action: {
+                    self.navigation.unwind()
+                }){
+                   Text("Back")
+                        .foregroundColor(Color.gray)
+                        .fontWeight(.semibold)
+                        .font(.custom("Poppins", size: 24))
+                }
+                Spacer()
+            }.padding()
+            VStack(){
+                VStack(alignment: .leading) {
+                    Image("bose")
+                        .resizable()
+                        .frame(maxWidth: 350, maxHeight: 200)
+                    Text("Bose QuietComfort 100")
+                        .h1()
+                    HStack(alignment: .top){
+                        VStack(alignment: .leading){
+                             Text("Ticket Price: ")
+                               .h2()
+                            Text("$15.00")
+                                .foregroundColor(Color("PurpleBlue"))
+                        }
+                       
+                        Spacer()
+                        VStack(alignment: .trailing){
+                            Text("Time Remaining:")
+                                .h2()
+                            Text("00:45:31")
+                                .foregroundColor(Color.red)
+                        }
+                    }
+                    
+                    Rectangle()
+                        .frame(height: 1.0, alignment: .bottom)
+                        .foregroundColor(Color("LightGray"))
+                    
+                    HStack(){
+                        Text("8")
+                            .fontWeight(.bold)
+                            .foregroundColor(Color.white)
+                            .padding(10)
+                            .background(RoundedRectangle(cornerRadius: 2)
+                                .foregroundColor(Color("PurpleBlue")))
+                        Text("Tickets Left")
+                        Spacer()
+                        VStack(alignment: .trailing){
+                            Text("Posted 5 Days Ago")
+                            Text("Seller: Jennifer Smith")
+                        }
+                    }
+                    
+                    Rectangle()
+                       .frame(height: 1.0, alignment: .bottom)
+                       .foregroundColor(Color("LightGray"))
+                    
                     VStack(alignment: .leading){
-                         Text("Ticket Price: ")
-                           .h2()
-                        Text("$15.00")
-                            .foregroundColor(Color("PurpleBlue"))
-                    }
-                   
-                    Spacer()
-                    VStack(alignment: .trailing){
-                        Text("Time Remaining:")
-                            .h2()
-                        Text("00:45:31")
-                            .foregroundColor(Color.red)
+                        Text("Description:")
+                            .fontWeight(.bold)
+                        Text("Lorem Ipsum dolor set amet.")
                     }
                 }
-                
-                Rectangle()
-                    .frame(height: 1.0, alignment: .bottom)
-                    .foregroundColor(Color("LightGray"))
-                
-                HStack(){
-                    Text("8")
-                        .fontWeight(.bold)
-                        .foregroundColor(Color.white)
-                        .padding(10)
-                        .background(RoundedRectangle(cornerRadius: 2)
-                            .foregroundColor(Color("PurpleBlue")))
-                    Text("Tickets Left")
-                    Spacer()
-                    VStack(alignment: .trailing){
-                        Text("Posted 5 Days Ago")
-                        Text("Seller: Jennifer Smith")
-                    }
-                }
-                
-                Rectangle()
-                   .frame(height: 1.0, alignment: .bottom)
-                   .foregroundColor(Color("LightGray"))
-                
-                VStack(alignment: .leading){
-                    Text("Description:")
-                        .fontWeight(.bold)
-                    Text("Lorem Ipsum dolor set amet.")
-                }
-            }
-            Spacer()
-            VStack(alignment: .center){
-                Text("How many tickets would you like to purchase?:")
+                Spacer()
+                VStack(alignment: .center){
+                    Text("How many tickets would you like to purchase?:")
 
-                Text("1")
-                    .fontWeight(.bold)
-            }.padding(20)
-            Button(action:{
-               
-            }){
-                Text("Buy Now")
-                    .blueButtonText()
-            }.buttonStyle(BigBlueButtonStyle())
-        }.padding(40)
+                    Text("1")
+                        .fontWeight(.bold)
+                }.padding(20)
+                
+                Button(action:{
+                    self.navigation.success()
+                }){
+                    Text("Buy Now")
+                        .blueButtonText()
+                }.buttonStyle(BigBlueButtonStyle())
+            }.padding(40)
+        }
     }
 }
 
