be202585d55bfa7a6553f1c9a5d18680a6f127b1
pmarks98 <piersonmarks@engineering.ucla.edu>
2020-03-08 16:40:05 -0700

    Small fix for item feed.

--- frontend/RaffleBay/RaffleBay/View/SaleItemTableView.swift
+++ frontend/RaffleBay/RaffleBay/View/SaleItemTableView.swift
@@ -14,7 +14,7 @@ struct GridStack<Content: View>: View {
     let rows: Int
     let columns: Int
     let content: (Int, Int) -> Content
-
+    
     var body: some View {
         VStack {
             ForEach(0 ..< rows, id: \.self) { row in
@@ -24,7 +24,7 @@ struct GridStack<Content: View>: View {
                     }
                 }
             }
-        }
+        }.frame(height: CGFloat(200 * rows))
     }
 
     init(rows: Int, columns: Int, @ViewBuilder content: @escaping (Int, Int) -> Content) {
