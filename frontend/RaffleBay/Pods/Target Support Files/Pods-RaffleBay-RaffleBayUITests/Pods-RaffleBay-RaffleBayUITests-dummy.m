5ea5da6dad2a82e644cf8cdd481d506d7111ffe8
Meera Rachamallu <mrachamallu@gmail.com>
2020-03-09 10:27:35 -0700

    modified .gitignore

--- frontend/RaffleBay/Pods/Target Support Files/Pods-RaffleBay-RaffleBayUITests/Pods-RaffleBay-RaffleBayUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_RaffleBay_RaffleBayUITests : NSObject
-@end
-@implementation PodsDummy_Pods_RaffleBay_RaffleBayUITests
-@end
--- frontend/RaffleBay/Pods/Target Support Files/Pods-RaffleBay/Pods-RaffleBay-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_RaffleBay : NSObject
-@end
-@implementation PodsDummy_Pods_RaffleBay
-@end
